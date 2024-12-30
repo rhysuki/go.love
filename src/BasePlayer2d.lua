86c9f6b39eaec6b0f9e42a79ff564f54015dbdff
rhy <rhynomatty100@gmail.com>
2024-12-30 10:30:43 -0300

    Rewrite BasePlayer2d

--- src/BasePlayer2d.lua
+++ src/BasePlayer2d.lua
@@ -1,54 +1,49 @@
 local Hitbox = require("src.Hitbox")
 ---@class BasePlayer2d: Hitbox
+---@overload fun(x: number?, y:number?): BasePlayer2d
 local BasePlayer2d = Hitbox:extend()
 
 function BasePlayer2d:new(x, y)
 	BasePlayer2d.super.new(self, x, y, 8, 13, {"player"}, {"wall"})
 
-	-- If `false`, this player won't respond to inputs and will just stay idle.
-	self.is_input_enabled = true
 	self.speed = 150
-	---@type "right" | "left"
-	self.facing_direction = "right"
 	self.debug_draw_mode = "line"
+	self._is_flipped = false
 	self._gravity = 12
 	self._jump_velocity = -5
-	self._animation = ANIMATIONS.player_idle_right
+	self._animation = ANIMATIONS.player_idle
 end
 
 function BasePlayer2d:update(dt)
-	local x = self.is_input_enabled and INPUT:get("move") or 0
+	-- Move
+	local move_x = INPUT:get("move")
 
-	if x > 0 then
-		x = 1
-	elseif x < 0 then
-		x = -1
-	end
-
-	self.vx = x * self.speed * dt
+	self.vx = self.speed * move_x * dt
 	self.vy = self.vy + self._gravity * dt
 
-	if self.is_grounded and INPUT:pressed("confirm") and self.is_input_enabled then
+	-- Jump
+	if self.is_grounded and INPUT:pressed("confirm") then
 		self.vy = self._jump_velocity
-		SOUNDS.select2:play()
 	end
 
-	BasePlayer2d.super.update(self, dt)
+	self:move_and_collide()
 
-	if x ~= 0 then
-		self.facing_direction = x > 0 and "right" or "left"
-	end
-
+	-- Update animations
 	if self.is_grounded then
-		self._animation = self.vx ~= 0 and ANIMATIONS.player_run or ANIMATIONS.player_idle_right
+		self._animation = move_x == 0 and ANIMATIONS.player_idle or ANIMATIONS.player_run
 	else
 		self._animation = self.vy < 0 and ANIMATIONS.player_jump or ANIMATIONS.player_fall
+	end
+
+	if move_x ~= 0 then
+		self._is_flipped = move_x < 0
 	end
 end
 
 function BasePlayer2d:draw()
-	local flip = self.facing_direction ~= "right"
-	self._animation:draw(self.x - 4 + (flip and 16 or 0), self.y - 3, 0, flip and -1 or 1, 1)
+	local scale = self._is_flipped and -1 or 1
+	local x_offset = self._is_flipped and 16 or 0
+	self._animation:draw(self.x - 4 + x_offset, self.y - 3, 0, scale, 1)
 	BasePlayer2d.super.draw(self)
 end
 

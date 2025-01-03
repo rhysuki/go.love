7b605d787857e769fcbb7dc76cb82fde76b78257
rhy <rhynomatty100@gmail.com>
2025-01-02 22:28:11 -0300

    Add an alternate player

--- assets/data/collections/animations.lua
+++ assets/data/collections/animations.lua
@@ -5,6 +5,7 @@
 
 local anim8 = require("lib.anim8.anim8")
 local player_atlas = love.graphics.newImage("assets/images/player.png")
+local player_alt_atlas = love.graphics.newImage("assets/images/player_alt.png")
 local player_grid = anim8.newGrid(16, 16, player_atlas:getWidth(), player_atlas:getHeight())
 
 ---Draw this animation. All extra arguments, like position, size, offset, etc,
@@ -41,36 +42,41 @@ local function update_animations(t, dt)
 	end
 end
 
-local animations = {
-	player = {
+local function create_player_animation_table(atlas)
+	return {
 		-- General-purpose and topdown-focused animations
-		idle_down = create_animation(player_atlas, player_grid("2-2", 1), 0.2),
-		idle_right = create_animation(player_atlas, player_grid("2-2", 2), 0.2),
-		idle_left = create_animation(player_atlas, player_grid("2-2", 3), 0.2),
-		idle_up = create_animation(player_atlas, player_grid("2-2", 4), 0.2),
+		idle_down = create_animation(atlas, player_grid("2-2", 1), 0.2),
+		idle_right = create_animation(atlas, player_grid("2-2", 2), 0.2),
+		idle_left = create_animation(atlas, player_grid("2-2", 3), 0.2),
+		idle_up = create_animation(atlas, player_grid("2-2", 4), 0.2),
 
-		walk_down = create_animation(player_atlas, player_grid("1-4", 1), 0.2),
-		walk_right = create_animation(player_atlas, player_grid("1-4", 2), 0.2),
-		walk_left = create_animation(player_atlas, player_grid("1-4", 3), 0.2),
-		walk_up = create_animation(player_atlas, player_grid("1-4", 4), 0.2),
+		walk_down = create_animation(atlas, player_grid("1-4", 1), 0.2),
+		walk_right = create_animation(atlas, player_grid("1-4", 2), 0.2),
+		walk_left = create_animation(atlas, player_grid("1-4", 3), 0.2),
+		walk_up = create_animation(atlas, player_grid("1-4", 4), 0.2),
 
-		push_down = create_animation(player_atlas, player_grid("1-4", 5), 0.2),
-		push_right = create_animation(player_atlas, player_grid("1-4", 6), 0.2),
-		push_left = create_animation(player_atlas, player_grid("1-4", 7), 0.2),
-		push_up = create_animation(player_atlas, player_grid("1-4", 8), 0.2),
+		push_down = create_animation(atlas, player_grid("1-4", 5), 0.2),
+		push_right = create_animation(atlas, player_grid("1-4", 6), 0.2),
+		push_left = create_animation(atlas, player_grid("1-4", 7), 0.2),
+		push_up = create_animation(atlas, player_grid("1-4", 8), 0.2),
 
-		victory = create_animation(player_atlas, player_grid("1-14", 14), 0.05),
-		victory_pose = create_animation(player_atlas, player_grid("1-1", 15), 0.05),
+		victory = create_animation(atlas, player_grid("1-14", 14), 0.05),
+		victory_pose = create_animation(atlas, player_grid("1-1", 15), 0.05),
 
 		-- Sidescrolling-focused animations. To flip them, flip the underlying object.
-		-- Example: `animations.player_run.data:flipH()`
-		idle = create_animation(player_atlas, player_grid("2-2", 9), 0.1),
-		run = create_animation(player_atlas, player_grid("1-4", 9), 0.1),
-		jump = create_animation(player_atlas, player_grid("1-1", 10), 0.1),
-		fall = create_animation(player_atlas, player_grid("1-1", 11), 0.1),
-		crouch = create_animation(player_atlas, player_grid("1-1", 12), 0.1),
-		item = create_animation(player_atlas, player_grid("1-1", 13), 0.1),
+		-- Example: `animations.run.data:flipH()`
+		idle = create_animation(atlas, player_grid("2-2", 9), 0.1),
+		run = create_animation(atlas, player_grid("1-4", 9), 0.1),
+		jump = create_animation(atlas, player_grid("1-1", 10), 0.1),
+		fall = create_animation(atlas, player_grid("1-1", 11), 0.1),
+		crouch = create_animation(atlas, player_grid("1-1", 12), 0.1),
+		item = create_animation(atlas, player_grid("1-1", 13), 0.1),
 	}
+end
+
+local animations = {
+	player = create_player_animation_table(player_atlas),
+	player_alt = create_player_animation_table(player_alt_atlas),
 }
 
 ---Update every registered animation. Should be called only once per frame.

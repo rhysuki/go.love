bb8b686c3ac3e29765e01a398a627aa89610ddc8
rhy <rhynomatty100@gmail.com>
2025-03-07 19:37:31 -0300

    Move Window:setup() as early as possible

--- main.lua
+++ main.lua
@@ -1,14 +1,17 @@
+local Window = require("src.singleton.Window")
+---Setup Window ASAP, as some things rely on window state to load properly
+Window:setup(3)
+
 local push = require("lib.push.push")
 local timer = require("lib.hump.timer")
 local log = require("lib.log.log")
 local colors = require("assets.data.collections.colors")
-local Window = require("src.singleton.Window")
 local Input = require("src.singleton.Input")
 local Debug = require("src.singleton.Debug")
-local Camera
-local Node
-local root
-local animations
+local Camera = require("src.singleton.Camera")
+local Node = require("src.Node")
+local animations = require("assets.data.collections.animations")
+local root = Node()
 
 ---Draw function to pass to the Camera singleton, to avoid creating a new function
 ---every frame.
@@ -18,17 +21,8 @@ local function draw()
 end
 
 function love.load()
-	Window:setup(3)
-	-- The Camera singleton MUST be required AFTER Window:setup(), since it relies
-	-- on LÖVE's window state.
-	Camera = require("src.singleton.Camera")
 	love.window.setTitle("Project Skeleton")
 	push:setBorderColor(colors.b16_black)
-
-	Node = require("src.Node")
-	root = Node()
-	animations = require("assets.data.collections.animations")
-
 	require("globals")
 	require("demo")(root, "topdown")
 	log.info("Finished loading")
